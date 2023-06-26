d18124eab792147e47c2c84bd1b5101f0906007e
Patambuss <121962154+Patambuss@users.noreply.github.com>
2023-06-26 17:05:51 -0400

    Create client.cpp

---
+++ client.cpp
@@ -0,0 +1,46 @@
+ asio::ip::tcp::resolver resolver(ioContext);
+        asio::ip::tcp::resolver::results_type endpoints = resolver.resolve("localhost", "8000");
+
+        asio::ip::tcp::socket socket(ioContext);
+        asio::connect(socket, endpoints);
+
+        std::cout << "Connected to the chat server\n";
+
+        std::thread readThread([&socket]() {
+            try
+            {
+                asio::streambuf buffer;
+                while (true)
+                {
+                    // Read message from the server
+                    asio::read_until(socket, buffer, '\n');
+                    std::string message(asio::buffers_begin(buffer.data()), asio::buffers_end(buffer.data()));
+                    buffer.consume(buffer.size());
+
+                    // Print the received message
+                    std::cout << message;
+                }
+            }
+            catch (const std::exception& ex)
+            {
+                std::cerr << "Exception in readThread: " << ex.what() << std::endl;
+            }
+        });
+
+        // Send messages to the server
+        std::string message;
+        while (std::getline(std::cin, message))
+        {
+            message += '\n';
+            asio::write(socket, asio::buffer(message));
+        }
+
+        readThread.join();
+    }
+    catch (const std::exception& ex)
+    {
+        std::cerr << "Exception in main: " << ex.what() << std::endl;
+    }
+
+    return 0;
+}
