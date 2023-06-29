f3ec25609aa8e4fb6128855df03ab11c657b27cc
Patambuss <121962154+Patambuss@users.noreply.github.com>
2023-06-29 09:55:45 -0400

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -1,4 +1,17 @@
- asio::ip::tcp::resolver resolver(ioContext);
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/asio.hpp>
+#include <iostream>
+#include <thread>
+#include <vector>
+
+using namespace boost;
+
+
+int main() {
+    try {
+        asio::io_context ioContext;
+
+        asio::ip::tcp::resolver resolver(ioContext);
         asio::ip::tcp::resolver::results_type endpoints = resolver.resolve("localhost", "8000");
 
         asio::ip::tcp::socket socket(ioContext);
@@ -7,11 +20,9 @@
         std::cout << "Connected to the chat server\n";
 
         std::thread readThread([&socket]() {
-            try
-            {
+            try {
                 asio::streambuf buffer;
-                while (true)
-                {
+                while (true) {
                     // Read message from the server
                     asio::read_until(socket, buffer, '\n');
                     std::string message(asio::buffers_begin(buffer.data()), asio::buffers_end(buffer.data()));
@@ -20,27 +31,24 @@
                     // Print the received message
                     std::cout << message;
                 }
-            }
-            catch (const std::exception& ex)
-            {
+            } catch (const std::exception& ex) {
                 std::cerr << "Exception in readThread: " << ex.what() << std::endl;
             }
         });
 
         // Send messages to the server
         std::string message;
-        while (std::getline(std::cin, message))
-        {
+        while (std::getline(std::cin, message)) {
             message += '\n';
             asio::write(socket, asio::buffer(message));
         }
 
         readThread.join();
-    }
-    catch (const std::exception& ex)
-    {
+    } catch (const std::exception& ex) {
         std::cerr << "Exception in main: " << ex.what() << std::endl;
     }
 
     return 0;
 }
+
+
